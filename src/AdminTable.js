5f65976f8383304524cc46f5378f83d171b847c7
Anthoniselvi <anthoniselvi33@gmail.com>
2022-12-30 22:34:40 +0530

    backend updated

--- src/AdminTable.js
+++ src/AdminTable.js
@@ -1,53 +1,113 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import "./App.css";
 import Rating from "@mui/material/Rating";
+import axios from "axios";
 
-const getDataFromInputs = () => {
-  const data = localStorage.getItem("inputs");
-  if (data) {
-    return JSON.parse(data);
-  } else {
-    return [];
-  }
-};
+// const getDataFromInputs = () => {
+//   const data = localStorage.getItem("inputs");
+//   if (data) {
+//     return JSON.parse(data);
+//   } else {
+//     return [];
+//   }
+// };
 
 export default function AdminTable() {
-  const [inputs, setInputs] = useState(getDataFromInputs());
+  const [inputs, setInputs] = useState([]);
   // const [acceptedOrRejected, setAcceptedOrRejected] = useState("");
 
-  const updateAcceptance = (id, value) => {
-    console.log("selected ID: " + id);
-    const updatedArray = inputs.map((singleValue) => {
-      console.log(id, singleValue.id);
-      if (singleValue.id === id) {
-        return {
-          id: singleValue.id,
-          name: singleValue.name,
-          mobile: singleValue.mobile,
-          rating: singleValue.rating,
-          yesOrNo: singleValue.yesOrNo,
-          comments: singleValue.comments,
-          accepted: value,
-          editOrDelete: "",
-        };
-      } else {
-        return singleValue;
-      }
+  const fetchAllFeedbacks = () => {
+    axios.get("http://localhost:2000/feedback").then((response) => {
+      // console.log(response);
+      console.log(response.data);
+      setUpdatedFeedbacks(response.data);
     });
-
-    setInputs(updatedArray);
-    localStorage.setItem("inputs", JSON.stringify(updatedArray));
   };
 
-  const deleteRow = (id) => {
-    const filteredRow = inputs.filter((input) => {
-      return input.id !== id;
-    });
-    setInputs(filteredRow);
-    localStorage.setItem("inputs", JSON.stringify(filteredRow));
+  const updateAcceptance = (feedbackToUpdate, value) => {
+    axios
+      .put("http://localhost:2000/feedback", {
+        id: parseInt(feedbackToUpdate.id),
+        name: feedbackToUpdate.name,
+        mobile: feedbackToUpdate.mobile,
+        rating: parseInt(feedbackToUpdate.rating),
+        recommendation: feedbackToUpdate.recommendation === "Yes" ? 1 : 0,
+        comments: feedbackToUpdate.comments,
+        status: value,
+      })
+      .then((response) => {
+        fetchAllFeedbacks();
+      });
   };
+
+  const deleteRow = (idToDelete) => {
+    console.log(idToDelete);
+    console.log(parseInt(idToDelete));
+    axios
+      .delete("http://localhost:2000/feedback", {
+        data: {
+          id: parseInt(idToDelete),
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        fetchAllFeedbacks();
+      });
+  };
+
+  // const updateAcceptance = (id, value) => {
+  //   console.log("selected ID: " + id);
+  //   const updatedArray = inputs.map((singleValue) => {
+  //     console.log(id, singleValue.id);
+  //     if (singleValue.id === id) {
+  //       return {
+  //         id: singleValue.id,
+  //         name: singleValue.name,
+  //         mobile: singleValue.mobile,
+  //         rating: singleValue.rating,
+  //         recommendation: singleValue.recommendation,
+  //         comments: singleValue.comments,
+  //         accepted: value,
+  //         editOrDelete: "",
+  //       };
+  //     } else {
+  //       return singleValue;
+  //     }
+  //   });
+
+  //   setInputs(updatedArray);
+  //   localStorage.setItem("inputs", JSON.stringify(updatedArray));
+  // };
+
+  // const deleteRow = (id) => {
+  //   const filteredRow = inputs.filter((input) => {
+  //     return input.id !== id;
+  //   });
+  //   setInputs(filteredRow);
+  //   localStorage.setItem("inputs", JSON.stringify(filteredRow));
+  // };
+
+  const setUpdatedFeedbacks = (responseData) => {
+    const feedbackList = responseData.map((singleFeedback) => {
+      return {
+        id: singleFeedback.id,
+        name: singleFeedback.name,
+        mobile: singleFeedback.mobile,
+        rating: singleFeedback.rating,
+        recommendation: singleFeedback.recommendation === 0 ? "No" : "Yes",
+        comments: singleFeedback.comments,
+        status: singleFeedback.status,
+      };
+    });
+
+    setInputs(feedbackList);
+  };
+
+  useEffect(() => {
+    fetchAllFeedbacks();
+  }, []);
 
   return (
     <div className="feedback-container">
@@ -58,9 +118,9 @@ export default function AdminTable() {
             <th>Name</th>
             <th>Mobile</th>
             <th>StarRating</th>
-            <th>Recommended</th>
+            <th>Recommendated</th>
             <th>Comments</th>
-            <th>Accepted</th>
+            <th>Status</th>
             <th>Remarks</th>
           </tr>
         </thead>
@@ -72,13 +132,13 @@ export default function AdminTable() {
               <td>
                 <Rating value={input.rating} readOnly />
               </td>
-              <td>{input.yesOrNo}</td>
+              <td>{input.recommendation}</td>
               <td>{input.comments}</td>
               <td>
                 <div className="icon">
-                  {input.accepted === "Accepted" ? (
+                  {input.status === "Accepted" ? (
                     <p className="text-accepted">Accepted</p>
-                  ) : input.accepted === "Rejected" ? (
+                  ) : input.status === "Rejected" ? (
                     <p className="text-rejected">Rejected</p>
                   ) : (
                     <div className="icon">
@@ -89,7 +149,7 @@ export default function AdminTable() {
                           cursor: "pointer",
                         }}
                         // value={acceptedOrRejected}
-                        onClick={() => updateAcceptance(input.id, "Accepted")}
+                        onClick={() => updateAcceptance(input, "Accepted")}
                       />
                       <FaThumbsDown
                         style={{
@@ -98,7 +158,7 @@ export default function AdminTable() {
                           cursor: "pointer",
                         }}
                         // value={acceptedOrRejected}
-                        onClick={() => updateAcceptance(input.id, "Rejected")}
+                        onClick={() => updateAcceptance(input, "Rejected")}
                       />
                     </div>
                   )}
--- src/Form.js
+++ src/Form.js
@@ -5,62 +5,76 @@ import BasicRating from "./BasicRating";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+import axios from "axios";
 import Validation from "./Validation";
 
-const getDataFromInputs = () => {
-  const data = localStorage.getItem("inputs");
-  if (data) {
-    return JSON.parse(data);
-  } else {
-    return [];
-  }
-};
+// const getDataFromInputs = () => {
+//   const data = localStorage.getItem("inputs");
+//   if (data) {
+//     return JSON.parse(data);
+//   } else {
+//     return [];
+//   }
+// };
 export default function Form() {
-  const [inputs, setInputs] = useState(getDataFromInputs());
+  // const [inputs, setInputs] = useState([]);
   const [name, setName] = useState("");
   const [mobile, setMobile] = useState("");
   const [rating, setRating] = useState(1);
   const [comments, setComments] = useState("");
-  const [yesOrNo, setYesOrNo] = useState("");
-  const [style, setStyle] = useState(false);
-  const [errors, setErrors] = useState({});
-  const [dataIsCorrect, setDataIsCorrect] = useState(false);
+  const [recommendation, setRecommendation] = useState("");
+  // const [style, setStyle] = useState(false);
+  // const [errors, setErrors] = useState({});
+  // const [dataIsCorrect, setDataIsCorrect] = useState(false);
 
   const navigate = useNavigate();
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
-    console.log(name, mobile, rating, yesOrNo, comments);
-    setErrors(Validation(inputs));
-    setDataIsCorrect(true);
+    console.log(name, mobile, rating, recommendation, comments);
+    // setErrors(Validation(inputs));
+    // setDataIsCorrect(true);
 
-    let newInputs = {
-      id: uuidv4(),
-      name,
-      mobile,
-      rating,
-      yesOrNo,
-      comments,
-      accepted: "",
-      editOrDelete: "",
-    };
+    axios
+      .post("http://localhost:2000/feedback", {
+        name: name,
+        mobile: mobile,
+        rating: parseInt(rating),
+        recommendation: recommendation === "Yes" ? 1 : 0,
+        comments: comments,
+      })
+      .then((response) => {
+        console.log(response);
+        navigate("/AdminTable");
+      });
+
+    // let newInputs = {
+    //   id: uuidv4(),
+    //   name,
+    //   mobile,
+    //   rating,
+    //   recommendation,
+    //   comments,
+    //   accepted: "",
+    //   editOrDelete: "",
+    // };
     setName("");
     setMobile("");
     setRating("");
     setComments("");
-    setYesOrNo("");
-    localStorage.setItem("inputs", JSON.stringify([...inputs, newInputs]));
-    console.log(inputs);
+    setRecommendation("");
+    // localStorage.setItem("inputs", JSON.stringify([...inputs, newInputs]));
+    // console.log(inputs);
     // navigate("/AdminPage");
     // navigate("/AdminTable");
   };
 
-  useEffect(() => {
-    if (Object.keys(errors).length === 0 && dataIsCorrect) {
-      alert("signup successfully");
-      navigate("/admintable");
-    }
-  }, [errors]);
+  // useEffect(() => {
+  //   if (Object.keys(errors).length === 0 && dataIsCorrect) {
+  //     alert("signup successfully");
+  //     navigate("/admintable");
+  //   }
+  // }, [errors]);
 
   return (
     <div>
@@ -76,7 +90,7 @@ export default function Form() {
             placeholder="Enter your Name"
           />
         </div>
-        {errors.name && <p className="error">{errors.name}</p>}
+        {/* {errors.name && <p className="error">{errors.name}</p>} */}
         <div className="row">
           <label>Mobile</label>
           <input
@@ -87,7 +101,7 @@ export default function Form() {
             placeholder="Enter your Mobile Number"
           />
         </div>
-        {errors.mobile && <p className="error">{errors.mobile}</p>}
+        {/* {errors.mobile && <p className="error">{errors.mobile}</p>} */}
         <div className="row">
           <label>How would you rate our service?</label>
           <Box
@@ -106,23 +120,23 @@ export default function Form() {
             />
           </Box>
         </div>
-        {errors.rating && <p className="error">{errors.rating}</p>}
+        {/* {errors.rating && <p className="error">{errors.rating}</p>} */}
         <div className="row">
           <label>Will you recommend us to Friends?</label>
           <div className="icon">
             <FaThumbsUp
-              style={{ color: yesOrNo === "yes" ? "green" : "black" }}
-              value={yesOrNo}
-              onClick={(e) => setYesOrNo("yes")}
+              style={{ color: recommendation === "yes" ? "green" : "black" }}
+              value={recommendation}
+              onClick={(e) => setRecommendation("yes")}
             />
             <FaThumbsDown
-              style={{ color: yesOrNo === "no" ? "red" : "black" }}
-              value={yesOrNo}
-              onClick={(e) => setYesOrNo("no")}
+              style={{ color: recommendation === "no" ? "red" : "black" }}
+              value={recommendation}
+              onClick={(e) => setRecommendation("no")}
             />
           </div>
         </div>
-        {errors.yesOrNo && <p className="error">{errors.yesOrNo}</p>}
+        {/* {errors.yesOrNo && <p className="error">{errors.yesOrNo}</p>} */}
         <div className="row">
           <label>Comments</label>
           <textarea
@@ -133,7 +147,7 @@ export default function Form() {
             placeholder="Enter your Comments"
           />
         </div>
-        {errors.comments && <p className="error">{errors.comments}</p>}
+        {/* {errors.comments && <p className="error">{errors.comments}</p>} */}
         <button>Submit</button>
       </form>
     </div>
